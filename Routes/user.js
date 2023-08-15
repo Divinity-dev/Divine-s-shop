626afa9aa89e2a2bc09a31c9523b6a43f771b662
Divinity-dev <divine_asiriuwa@yahoo.com>
2023-08-15 18:44:19 +0100

    Add user route

--- Routes/user.js
+++ Routes/user.js
@@ -1,13 +1,6 @@
 const router = require("express").Router();
 
 
-router.get("/test", (req, res) => {
-    res.send("test sucessful")
-})
-router.post("/usertest", (req, res) => {
-    const username = req.body.username;
-    res.send(username)
-    console.log(username)
-})
+
 
 module.exports = router;
