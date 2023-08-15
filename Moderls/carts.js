3d59d7f43458e52b130553439c9bf14df57064f3
Divinity-dev <divine_asiriuwa@yahoo.com>
2023-08-15 18:33:29 +0100

    Added cart model

--- Moderls/carts.js
+++ Moderls/carts.js
@@ -1,18 +1,18 @@
-// const mongoose = require("mongoose")
+const mongoose = require("mongoose")
 
-// const Cartschema = new mongoose.Schema(
-//   {
-//     UserId: {type: String, unique:true, required: true},
-//     Products: [
-//         {
-//             ProductId: { type:string},
-//             quantity: {type: Number, default: 1}
-//         }
-//     ]
+const Cartschema = new mongoose.Schema(
+  {
+    UserId: {type: String, unique:true, required: true},
+    Products: [
+        {
+            ProductId: { type:string},
+            quantity: {type: Number, default: 1}
+        }
+    ]
     
     
-//   },
-//   {timestamps: true}
-// )
+  },
+  {timestamps: true}
+)
 
-// module.exports = mongoose.model("Carts", Cartschema);
+module.exports = mongoose.model("Carts", Cartschema);
