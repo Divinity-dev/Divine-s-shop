794be3e6ba5286ecbd51c14a5c7e2e277878a418
Divinity-dev <divine_asiriuwa@yahoo.com>
2023-08-15 21:14:11 +0100

    add auth for login

--- Routes/auth.js
+++ Routes/auth.js
@@ -20,5 +20,26 @@ router.post("/register", async(req, res) => {
     }
 })
 
+// Login
+
+router.post("/login", async (req, res) => {
+try{
+  const user = await User.findOne({username: req.body.username})
+  if (!user) {
+    return res.status(404).json("No user with this name"); // Return early for invalid user
+}
+
+const decryptedPassword = CryptoJS.AES.decrypt(user.password, process.env.pass_key).toString(CryptoJS.enc.Utf8);
+
+if (decryptedPassword !== req.body.password) {
+    return res.status(401).json("Incorrect password"); // Return early for incorrect password
+}
+const {password, ...others} = user._doc;
+ res.status(200).json(others)
+}catch(err){
+    res.status(400).json(err)
+}
+})
+
 
 module.exports = router;
