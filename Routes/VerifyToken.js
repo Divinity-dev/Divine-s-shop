6bf772227815087b2141fdf45e9de30d9d5d795f
Divinity-dev <divine_asiriuwa@yahoo.com>
2023-08-16 23:38:01 +0100

    Add update funtion for users

--- Routes/VerifyToken.js
+++ Routes/VerifyToken.js
@@ -5,10 +5,12 @@ const jwt = require("jsonwebtoken")
 const verifyToken = (req, res, next) => {
     const authHeader = req.headers.token;
     if(authHeader){
+        const token = authHeader.split(" ")[1]
         jwt.verify(token, process.env.jwt_key, (err, data)=>{
             if(err){
                 res.status(401).json("token not valid")
             }
+            console.log(data);
             req.user = data
             next();
         })
--- Routes/user.js
+++ Routes/user.js
@@ -1,6 +1,21 @@
 const router = require("express").Router();
+const {verifyToken, verifyAuth} = require("./VerifyToken")
+const User = require("../models/User")
 
+router.put("/:id", verifyAuth, async (req, res)=>{
+  if(req.body.password){
+    req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.pass_key).toString()
+  }
+   try{
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body
+    }, {new:true})
+    res.status(200).json(updatedUser)
+   }catch(err){
+    res.status(400).json(err)
+   }
 
+})
 
 
 module.exports = router;
