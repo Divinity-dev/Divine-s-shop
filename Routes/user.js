5c0606c0ca1c8330b20ad701fbd33a64728658c5
Divinity-dev <divine_asiriuwa@yahoo.com>
2023-08-17 13:59:57 +0100

    Add function to get all users

--- Routes/user.js
+++ Routes/user.js
@@ -36,4 +36,14 @@ router.get("/:id", verifyAuthAndAdmin, async (req, res)=>{
   }
   })
 
+  // Get all users
+  router.get("/", verifyAuthAndAdmin, async (req, res)=>{
+    try{
+    const users = await User.find()
+    res.status(200).json(users)
+    }catch(err){
+      res.status(400).json(err)
+    }
+    })
+
 module.exports = router;
