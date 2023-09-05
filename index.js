ae1e1e94f0068b270eb3cf2672bd14f7ff0855a6
Divinity-dev <divine_asiriuwa@yahoo.com>
2023-09-05 23:21:48 +0100

    Add cors for external connection to api

--- index.js
+++ index.js
@@ -7,19 +7,22 @@ const authRoute = require("./Routes/auth")
 const productRoute = require("./Routes/Products")
 const CartRoute = require("./Routes/Carts")
 const OrderRoute = require("./Routes/Order")
+const cors = require("cors")
+const stripeRoute = require("./Routes/stripe")
 
 env.config()
 
 mongoose.connect(process.env.DB_URL)
 .then(()=>{console.log("successful")}).catch((err)=>{console.log(err)});
 
-
+app.use(cors())
 app.use(express.json())
 app.use("/api/auth", authRoute)
 app.use("/api/user", userRoute)
 app.use("/api/products", productRoute)
 app.use("/api/Cart", CartRoute)
 app.use("/api/Order", OrderRoute)
+app.use("/api/payment", stripeRoute)
 
 
 app.listen(5000, ()=>{
