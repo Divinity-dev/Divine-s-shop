46e554a24e27daea5952ab8e073d1da09919bbf0
Divinity-dev <divine_asiriuwa@yahoo.com>
2024-04-25 18:37:55 +0100

    Increased file upload limit

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ const CartRoute = require("./Routes/Carts")
 const OrderRoute = require("./Routes/Order")
 const cors = require("cors")
 const stripeRoute = require("./Routes/stripe")
+const multer = require("multer");
 
 env.config()
 
 mongoose.connect(process.env.DB_URL)
 .then(()=>{console.log("successful")}).catch((err)=>{console.log(err)});
 
+app.use(express.json({ limit: "50mb" }));
 app.use(cors())
 app.use(express.json())
 app.use("/api/auth", authRoute)
