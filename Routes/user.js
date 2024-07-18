4f3909c517af02ddef7114ebe1929c04a24a42c9
Divinity-dev <divine_asiriuwa@yahoo.com>
2024-07-18 11:23:05 +0100

    newest commit

--- Routes/user.js
+++ Routes/user.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const {verifyToken, verifyAuth, verifyAuthAndAdmin} = require("./VerifyToken")
-const User = require("../models/User")
+const User = require("../models/User.js")
 const CryptoJS = require("crypto-js")
 
 router.put("/:id", verifyAuth, async (req, res)=>{
